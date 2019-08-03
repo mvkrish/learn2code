d40efdcc1f664d3f8fa6623e3af398efc1e0d02b
Krish <krish@Krish-MBP.local>
2019-08-03 08:12:10 -0400

    initial commit

---
+++ assignment_1.py
@@ -0,0 +1,10 @@
+firstVariable='Ist Grade'
+secondVariable='2nd Grade'
+print(firstVariable, 'and', secondVariable)
+# it will print Ist Grade and 2nd Grade
+# do some code to swap the values of the variable
+
+
+
+print(firstVariable,'and', secondVariable)
+# it will print  2nd Grade and Ist Grade
